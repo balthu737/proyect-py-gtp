d98362cc49afdcb3c0ebc9cf29f6af9ae2825f63
balthu <balthu737@gmail.con>
2025-10-24 16:37:53 -0300

    mejorando la primera clase

--- 02-clases-en-py/01-clase-simple-persona-y-calculadora-de-edade/persona.py
+++ 02-clases-en-py/01-clase-simple-persona-y-calculadora-de-edade/persona.py
@@ -1,7 +1,9 @@
 class Persona:
     def __init__(self, nombre, año_de_nacimiento):
-        self.nombre = nombre
-        self.año_de_nacimiento = año_de_nacimiento
+        self._nombre = nombre
+        self._año_de_nacimiento = año_de_nacimiento
+    def __str__(self):
+        return f"Persona: {self._nombre}, nacido en {self._año_de_nacimiento}"
     def calcular_edad(self):
         año_actual = int(input("Que año es?: "))
         edad = año_actual- self.año_de_nacimiento
@@ -13,3 +15,4 @@ nombre = input("Cual es tu nombre?: ")
 edad = int(input("Cual estu edad?: "))
 persona = Persona(nombre, edad)
 #persona.presentacion()
+#print(persona)
